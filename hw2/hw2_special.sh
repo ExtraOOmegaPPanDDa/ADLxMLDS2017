e186c3076c90cdcb336f94072824b13ef081598a
OOmegaPPanDDa <b02705027@ntu.edu.tw>
2017-11-12 22:14:26 +0800

    hw2 special load model sh

--- hw2/hw2_special.sh
+++ hw2/hw2_special.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+wget https://www.dropbox.com/s/c98ckpv0rxbfefm/hw2_model_special.ckpt-345.data-00000-of-00001?dl=1 -O model_dir_special/hw2_model_special.ckpt-345.data-00000-of-00001
 python3 test_special.py $1 $2
